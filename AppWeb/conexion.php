4e7d0b62999f87eb2999246c843069474a828f7e
RVelez97 <59377999+RVelez97@users.noreply.github.com>
2020-01-18 16:55:29 -0500

    Update conexion.php

--- AppWeb/conexion.php
+++ AppWeb/conexion.php
@@ -1,6 +1,5 @@
 
-<!-- ingresa todos los datos referentes a tu conexion de base de datos
-de esta forma solo tendras que modificar un solo archivo -->
+//Credenciales para conectar la base de datos
 <?php
 $host_db = "localhost";
  $user_db = "rvb";
